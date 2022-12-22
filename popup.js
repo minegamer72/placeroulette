c560bfc7e42d58ffbc62fbedc2067999ebff6b40
codwarrior <108271839+codwarrior@users.noreply.github.com>
2022-12-22 14:34:13 -0500

    Update popup.js
    removed unnecessary wordage in commnents

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
 // Listen for clicks on the button
 document.getElementById("get-random-game-button").addEventListener("click", function() {
-  // Get a random game on the Roblox platform
+  // Get random game on Roblox
   getRandomGame();
 });
 
-// Get a random game on the Roblox platform
+// Get random game on Roblox
 function getRandomGame() {
   // Generate a random 9-10 digit number
   var gameId = Math.floor(100000000 + Math.random() * 900000000);
